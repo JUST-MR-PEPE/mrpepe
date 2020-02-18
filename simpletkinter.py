0e311c5263d1f3dca203aed0ed9ef3521d5af6c5
JUST-MR-PEPE <60903420+JUST-MR-PEPE@users.noreply.github.com>
2020-02-18 09:49:40 +0100

    Update simpletkinter.py

--- simpletkinter.py
+++ simpletkinter.py
@@ -3,20 +3,14 @@ from socket import*
 def getip():
     link = entry.get()
     host = gethostbyname(link)
-    #label2.configure(text="ip:"+host)
     entry.delete(0, 'end')
-    #entry.configure(text="ip:"+host)
-    insert(0, "a default value")
-
-
+    entry.insert(0,host)
 
 win = Tk()
 label = Label(win,text="get ip")
 entry = Entry(win,text="")
 button = Button(win,text="getip",command=getip) 
-label2 = Label(win,text="")
 label.pack()
 entry.pack()
-label2.pack()
 button.pack()
 win.mainloop()
